0e8a4e39f25a6203db7014b4bce00caa39ffdf3e
banana <watnirun03@gmail.com>
2026-02-16 10:50:01 +0700

    68010730

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -3,4 +3,18 @@ export class Person {
     public firstName: string;
     public lastName: string;
     public age: number;
-    
+    constructor(firstName: string, lastName: string, age: number) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+    public getFullName(): string {
+        return `${this.firstName} ${this.lastName}`;
+    }
+    public getAge(): number {
+        return this.age;
+    }
+    public setAge(newAge: number): void {
+        this.age = newAge;
+    }
+}
