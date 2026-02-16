64888727bda3ad1d5beb6fdbe8acb992541c0342
banana <watnirun03@gmail.com>
2026-02-16 10:47:48 +0700

    68010730

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,1 +1,6 @@
-export class Person {}
+export class Person {
+    public static COUNTRY: string = "Thailand";
+    public firstName: string;
+    public lastName: string;
+    public age: number;
+    
