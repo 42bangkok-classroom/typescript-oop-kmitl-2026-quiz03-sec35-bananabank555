b712556f2f9fb84bfd58eb57453d867c0386c0b9
banana <watnirun03@gmail.com>
2026-02-16 10:00:49 +0700

    68010730

--- src/practicals/p01/student.ts
+++ src/practicals/p01/student.ts
@@ -1,1 +1,11 @@
-export class Student {}
+export class Student {
+    firstname: string;
+    lastname: string;
+    constructor(firstname: string, lastname: string) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+    printName(): void {
+        console.log(`${this.firstname} ${this.lastname}`);
+    }
+}
